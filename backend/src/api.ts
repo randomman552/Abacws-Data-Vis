2ef65de7c71ba31a3e5370c0404f6fab9b011383
randomman552 <random.man552@protonmail.com>
2022-03-04 21:02:53 +0000

    Add openapi spec

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,11 +1,17 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { consoleLogErrors, errorHandler } from './middleware';
 import { devices, healthcheck } from './routers';
+import openapi from "./openapi.json"
 
 /** Express app */
 const api = express();
 // Api will only respond to JSON
 api.use(express.json());
+
+// Register root to return openapi spec
+api.all("/api", (req: Request, res: Response) => {
+    res.status(200).json(openapi);
+})
 
 // Register routes
 api.use("/api/healthcheck", healthcheck);
