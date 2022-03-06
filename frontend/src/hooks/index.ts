ec8482224d94d5c311234db6af6a58357c6cfed8
randomman552 <random.man552@protonmail.com>
2022-03-06 15:48:15 +0000

    Add device hooks

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,1 +1,2 @@
 export { useAPI, useAPISubscription } from "./useAPI"
+export { useDevices, useDeviceData, useDeviceHistory } from "./useDevice"
---
+++ frontend/src/hooks/useDevice.ts
@@ -0,0 +1,56 @@
+import { useAPI, useAPISubscription } from "./useAPI";
+
+// Types
+interface Position {
+    x: number
+    y: number
+    z: number
+}
+
+interface Device {
+    name: string
+    position: Position
+}
+
+interface Data {
+    timestamp: number
+    [fields: string]: unknown
+}
+
+interface DeviceData extends Device {
+    data: Data
+}
+
+interface DeviceHistory extends Device {
+    history: Data[]
+}
+
+// Hooks
+/**
+ * Hook to request list of devices from the API
+ */
+export function useDevices() {
+    return useAPI<{devices: Device[]}>("/api/devices")?.body.devices;
+}
+
+/**
+ * Hook to get the data corresponding to a device
+ * Uses {@link useAPISubscription}, so automaticaly updates
+ * @param deviceName The name of the device to query
+ * @returns 
+ */
+export function useDeviceData(deviceName: string) {
+    const url = `/api/devices/${deviceName}`;
+    return useAPISubscription<DeviceData>(url)?.body.data;
+}
+
+/**
+ * Hook to get the history
+ * Uses {@link useAPISubscription}, so automaticaly updates
+ * @param deviceName The name of the device we want to query
+ * @returns 
+ */
+export function useDeviceHistory(deviceName: string) {
+    const url = `/api/devices/${deviceName}/history`;
+    return useAPISubscription<DeviceHistory>(url)?.body.history;
+}
