33b997afa807542d5e87fadc039cbe88fca7eae2
randomman552 <random.man552@protonmail.com>
2022-03-22 18:43:53 +0000

    Remove duplicate openapi file

--- api/src/api/routers/docs.ts
+++ api/src/api/routers/docs.ts
@@ -1,9 +1,14 @@
 import express, { Response, Request } from "express";
 import swaggerUi from "swagger-ui-express"
-import openapi from "../openapi.json"
+import { readFileSync } from "fs"
+import { parse } from "yaml"
+
+// Read openapi.yaml
+const file = readFileSync("openapi.yaml", "utf-8");
+const openapi = parse(file);
+
 
 export const router = express.Router()
-
 
 router.all("/openapi.json", (_req: Request, res: Response) => {
     res.status(200).json(openapi);
