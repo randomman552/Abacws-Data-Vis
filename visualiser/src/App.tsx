b85c5fa1430daed237f536c1fe4f1c008b24be48
randomman552 <random.man552@protonmail.com>
2022-03-27 18:25:59 +0100

    Hide data panel by default on smaller screens

--- visualiser/src/App.tsx
+++ visualiser/src/App.tsx
@@ -4,7 +4,8 @@ import { useState } from 'react';
 import { HamburgerToggle } from './components';
 
 export default function App() {
-    const [hideDataView, setHideDataView] = useState(false)
+    // Defaults to hiden when window is less than 500px accross
+    const [hideDataView, setHideDataView] = useState(window.innerWidth < 500)
 
     return (
         <div className="app">
