908c679621988810a4129c3f6896611f7dda4fa4
randomman552 <random.man552@protonmail.com>
2022-03-28 14:33:32 +0100

    Add api key

--- api/src/api/constants.ts
+++ api/src/api/constants.ts
@@ -3,6 +3,7 @@ import { LogLevel } from "./types"
 export const PORT = Number(process.env.API_PORT) || 5000
 export const PRODUCTION = Boolean(process.env.PRODUCTION)
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/abacws"
+export const API_KEY = process.env.API_KEY || "V3rySecur3Pas3word"
 export const DEVICE_COLLECTION_PREFIX = "d"
 
 // Set LOG_LEVEL to info if we are not in a production environment, otherwise default to error
