190522769a87c231c88673985488715df51d5920
randomman552 <random.man552@protonmail.com>
2022-03-28 17:29:07 +0100

    Improve feedback from HamburgerToggle

--- visualiser/src/App.tsx
+++ visualiser/src/App.tsx
@@ -14,6 +14,7 @@ export default function App() {
             
             <HamburgerToggle
                 onClick={() => { setHideDataView(!hideDataView) }}
+                close={!hideDataView}
             />
             <DataView
                 hidden={hideDataView}
--- visualiser/src/components/Hamburger/Hamburger.tsx
+++ visualiser/src/components/Hamburger/Hamburger.tsx
@@ -2,11 +2,19 @@ import "./Hamburger.scss"
 
 export interface HamburgerToggleProps {
     onClick: () => void
+    /**
+     * Whether the HamburgerToggle will display a close icon instead of the 3 lines.
+     * Use with state from higher components to toggle the close state when the HamburgerToggle is clicked.
+     * If unset, this will have no effect.
+     */
+    close?: boolean
 }
 
-export function HamburgerToggle({ onClick }: HamburgerToggleProps) {
+export function HamburgerToggle({ onClick, close }: HamburgerToggleProps) {
+    const className = (close)? "hamburger-toggle close" : "hamburger-toggle"
+
     return (
-        <div onClick={onClick} className="hamburger-toggle">
+        <div onClick={onClick} className={className}>
             <div className="slice"></div>
             <div className="slice"></div>
             <div className="slice"></div>
